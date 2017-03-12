72b8632b34d95ee04231052612b6dd24eee807b1
simoniz0r <bnu.fantasma@gmail.com>
2017-03-12 06:02:02 -0500

    added space

--- vscodestatusbarcolor.sh
+++ vscodestatusbarcolor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Code from https://github.com/Microsoft/vscode/issues/1884#issuecomment-230790242
 echo "Input the color you would like to change the status bar in hex format"
-read -p "Color#" COLOR
+read -p "Color #" COLOR
 GETDIR="$(readlink -f $(which code))"
 DIR="${GETDIR::-9}"
     echo ".monaco-workbench>.part.statusbar{background-color:#$COLOR;}" | sudo tee -a "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
