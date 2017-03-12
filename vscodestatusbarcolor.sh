bb787a10272000b9cf953e3ded6f35bc1ad6cbd5
simoniz0r <bnu.fantasma@gmail.com>
2017-03-12 05:47:23 -0500

    Script now works with any install directory.

--- vscodestatusbarcolor.sh
+++ vscodestatusbarcolor.sh
@@ -2,6 +2,8 @@
 # Code from https://github.com/Microsoft/vscode/issues/1884#issuecomment-230790242
 echo "Input the color you would like to change the status bar in hex format"
 read -p "Color#" COLOR
-    echo ".monaco-workbench>.part.statusbar{background-color:#$COLOR;}" | sudo tee -a /usr/share/code/resources/app/out/vs/workbench/electron-browser/workbench.main.css
+GETDIR="$(readlink -f $(which code))"
+DIR="${GETDIR::-9}"
+    echo ".monaco-workbench>.part.statusbar{background-color:#$COLOR;}" | sudo tee -a "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
     echo "Status bar color changed to $COLOR!"
     exit 1
