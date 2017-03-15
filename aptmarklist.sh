c38c986abd8a92831cfd94e7937730f5aad8d510
simoniz0r <bnu.fantasma@gmail.com>
2017-03-14 21:16:31 -0500

    added origin url

--- aptmarklist.sh
+++ aptmarklist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # A script that writes the output apt-mark in alphabetical order to ~/packagelist.txt
+# Found here: http://askubuntu.com/questions/2389/generating-list-of-manually-installed-packages-and-querying-individual-packages
 comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) | tee ~/packagelist.txt
