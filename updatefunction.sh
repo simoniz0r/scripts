fe80ab85949d0df4285f097858f87018aa8c8dab
simoniz0r <bnu.fantasma@gmail.com>
2017-03-17 00:31:28 -0500

    sample function to update scripts easily updates scripts based on version number and update notes stored in remote file.

--- updatefunction.sh
+++ updatefunction.sh
@@ -5,19 +5,43 @@
 DDVER="1.3.3"
 SCRIPTNAME="$0"
 
+updatescript () {
+cat >/tmp/updatescript.sh <<EOL
+rm -f $SCRIPTNAME
+wget -O $SCRIPTNAME "https://raw.githubusercontent.com/simoniz0r/discorddownloader/master/discorddownloader.sh"
+chmod +x $SCRIPTNAME
+rm -f /tmp/updatescript.sh
+exec $SCRIPTNAME
+exit 0
+EOL
+}
+
 updatecheck () {
     echo "Checking for new version..."
-    VERTEST=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/discorddownloader/master/discorddownloader.sh 2>&1 | grep DDVER= | tr -d 'DDVER="')
+    UPNOTES=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/discorddownloader/master/version.txt 2>&1 | grep X= | tr -d 'X="')
+    VERTEST=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/discorddownloader/master/version.txt 2>&1 | grep DDVER= | tr -d 'DDVER="')
     if [[ $DDVER != $VERTEST ]]; then
-        echo "There is a new version of discorddownloader available; updating now."
-        echo "Creating update script..."
-        echo "rm -f $SCRIPTNAME /n wget -o https://raw.githubusercontent.com/simoniz0r/discorddownloader/master/discorddownloader.sh $SCRIPTNAME /n chmod +x $SCRIPTNAME /n rm -f ~/Downloads/updatescript.sh /n exec $SCRIPTNAME /n exit 0" > ~/Downloads/updatescript.sh
-        chmod +x ~/Downloads/updatescript.sh
-        echo "Running update script..."
-        exec ~/Downloads/updatescript.sh
-        exit 0
+        echo "Installed version: $DDVER -- Current version: $VERTEST"
+        echo "A new version is available!"
+        echo $UPNOTES
+        read -p "Would you like to update? Y/N " -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo
+            echo "Creating update script..."
+            updatescript
+            chmod +x /tmp/updatescript.sh
+            echo "Running update script..."
+            exec /tmp/updatescript.sh
+            exit 0
+        else
+            echo
+            main
+        fi
     else
+        echo "Installed version: $DDVER -- Current version: $VERTEST"
         echo "discorddownloader is up to date."
+        echo
+        main
     fi
 }
 
