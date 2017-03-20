66fbcd942b10730e5820a70cd3f3b6ca54a71395
simoniz0r <bnu.fantasma@gmail.com>
2017-03-20 03:17:31 -0500

    added description

--- vscodestatusbarcolor.sh
+++ vscodestatusbarcolor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Code from https://github.com/Microsoft/vscode/issues/1884#issuecomment-230790242
+# A quick and semi-dirty script that will append the color code of your choice to VSCode's CSS file to change the color of the status bar.
+# Make a backup of your 'workbench.main.css' file before using this, and only use it once per update/install.
+
 echo "Input the color you would like to change the status bar in hex format"
 read -p "Color #" COLOR
 GETDIR="$(readlink -f $(which code))"
