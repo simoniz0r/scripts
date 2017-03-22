50455e3874be902cdbaf21129e4d4dbd680895f3
simoniz0r <bnu.fantasma@gmail.com>
2017-03-21 23:04:33 -0500

    AMVER -> STVER

--- speedtest.sh
+++ speedtest.sh
@@ -34,9 +34,9 @@ EOL
 updatecheck () {
     echo "Checking for new version..."
     UPNOTES=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/UsefulScripts/master/speedtestversion.txt 2>&1 | grep X= | tr -d 'X="')
-    VERTEST=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/UsefulScripts/master/speedtestversion.txt 2>&1 | grep AMVER= | tr -d 'AMVER="')
-    if [[ $AMVER != $VERTEST ]]; then
-        echo "Installed version: $AMVER -- Current version: $VERTEST"
+    VERTEST=$(curl -v --silent https://raw.githubusercontent.com/simoniz0r/UsefulScripts/master/speedtestversion.txt 2>&1 | grep STVER= | tr -d 'STVER="')
+    if [[ $STVER != $VERTEST ]]; then
+        echo "Installed version: $STVER -- Current version: $VERTEST"
         echo "A new version is available!"
         echo $UPNOTES
         read -p "Would you like to update? Y/N " -n 1 -r
@@ -53,7 +53,7 @@ updatecheck () {
             echo "speedtest.sh was not updated."
         fi
     else
-        echo "Installed version: $AMVER -- Current version: $VERTEST"
+        echo "Installed version: $STVER -- Current version: $VERTEST"
         echo $UPNOTES
         echo "speedtest.sh is up to date."
     fi
