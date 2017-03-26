48a07d6a736bbc8f9697afbb4ce52b68b9e35f06
simoniz0r <bnu.fantasma@gmail.com>
2017-03-26 04:13:51 -0500

    changed password copied message to be more clear

--- gpgpassman.sh
+++ gpgpassman.sh
@@ -251,7 +251,7 @@ main () {
                     echo "Wrong password or gpg failure!"
                     exit 0
                 fi
-                echo "Copying password to clipboard for 45 seconds..."
+                echo "$SERVNAME password to clipboard; clipboard will be cleared after 45 seconds..."
                 sleep 45
                 echo -n "Password cleared from clipboard" | xclip -selection c -i
                 echo "Password cleard from clipboard."
@@ -263,7 +263,7 @@ main () {
                     main "dec"
                     exit 0
                 fi
-                zenity --forms --timeout=45 --text="Password copied to clipboard for 45 seconds..." --cancel-label="Clear now and return to main" --ok-label="Clear now and close"
+                zenity --forms --timeout=45 --text="Password to clipboard; clipboard will be cleared after 45 seconds..." --cancel-label="Clear now and return to main" --ok-label="Clear now and close"
                 if [[ $? -eq 1 ]]; then
                     echo -n "Password cleared from clipboard" | xclip -selection c -i
                     SERVNAME=""
