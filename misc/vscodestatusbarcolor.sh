27f9fc9a4b1932edd6aea1137b346c736763f661
simoniz0r <bnu.fantasma@gmail.com>
2017-04-02 18:58:32 -0500

    backup file to ~/.vscode; use --revert to restore

--- misc/vscodestatusbarcolor.sh
+++ misc/vscodestatusbarcolor.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # A script that uses 'sed' to replace all colors matching '#007acc' with the hex color of the user's choice.
 # This will replace all '#007acc', so there may be some other places that get recolored also (such changes to files on the sidebar)
-# Make a backup of your 'workbench.main.css' file before using this in case you are not happy with the changes.
+# A backup copy of the 'workbench.main.css' file will be saved in ~/.vscode/workbench.main.css.backup in case you don't like the changes.
+# Use '--revert' to replace the original 'workbench.main.css' file.
+GETDIR="$(readlink -f $(which code))"
+DIR="${GETDIR::-9}"
 
+if [ "$1" = "--revert" ];then
+    sudo cp ~/.vscode/workbench.main.css.backup "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
+    echo "Original workbench file replaced."
+    exit 0
+fi
 echo "Input the color you would like to change the status bar in hex format"
 read -p "Color #" -n 6 -r
 echo
@@ -14,8 +22,10 @@ if [ "${REPLY: -1}" = "#" ];then
     echo "Invalid input; 6 digit hex color code; do not include the '#'"
     exit 0
 fi
-GETDIR="$(readlink -f $(which code))"
-DIR="${GETDIR::-9}"
-    sudo sed -i -e 's/007acc/'$REPLY'/g' "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
-    echo "Status bar color changed to #$REPLY!"
-    exit 1
+if [ ! -d ~/.vscode ];then
+    mkdir ~/.vscode
+fi
+sudo cp "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css ~/.vscode/workbench.main.css.backup
+sudo sed -i -e 's/007acc/'$REPLY'/g' "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
+echo "Status bar color changed to #$REPLY!"
+exit 1
