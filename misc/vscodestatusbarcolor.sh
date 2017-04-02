5ca5a5e4ccd05d5116f8a6ec56097052537ffe33
simoniz0r <bnu.fantasma@gmail.com>
2017-04-02 18:25:41 -0500

    Made script less hackish by using sed to replace 007acc

--- misc/vscodestatusbarcolor.sh
+++ misc/vscodestatusbarcolor.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# Code from https://github.com/Microsoft/vscode/issues/1884#issuecomment-230790242
-# A quick and semi-dirty script that will append the color code of your choice to VSCode's CSS file to change the color of the status bar.
-# Make a backup of your 'workbench.main.css' file before using this, and only use it once per update/install.
+# A script that uses 'sed' to replace all colors matching '#007acc' with the hex color of the user's choice.
+# This will replace all '#007acc', so there may be some other places that get recolored also (such changes to files on the sidebar)
+# Make a backup of your 'workbench.main.css' file before using this in case you are not happy with the changes.
 
 echo "Input the color you would like to change the status bar in hex format"
-read -p "Color #" COLOR
+read -p "Color #" -n 6 -r
+echo
+if [ -z "$REPLY" ]; then
+    echo "No color input"
+    exit 0
+fi
+if [ "${REPLY: -1}" = "#" ];then
+    echo "Invalid input; 6 digit hex color code; do not include the '#'"
+    exit 0
+fi
 GETDIR="$(readlink -f $(which code))"
 DIR="${GETDIR::-9}"
-    echo ".monaco-workbench>.part.statusbar{background-color:#$COLOR;}" | sudo tee -a "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
-    echo "Status bar color changed to #$COLOR!"
+    sudo sed -i -e 's/007acc/'$REPLY'/g' "$DIR"/resources/app/out/vs/workbench/electron-browser/workbench.main.css
+    echo "Status bar color changed to #$REPLY!"
     exit 1
