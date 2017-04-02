0fa8404c7d657636b0b0a4a24ffc3d42956591fa
simoniz0r <bnu.fantasma@gmail.com>
2017-04-02 06:31:38 -0500

    added checks for curl and zenity

--- misc/lyrics.sh
+++ misc/lyrics.sh
@@ -4,13 +4,36 @@
 # Idea from https://gist.github.com/febuiles/1549991
 # Written by simonizor 4/2/2017
 
-if [ -z "$1" ]; then
-	artist=$(zenity --entry --title=Artist --text="Enter the artist name:")
-	title=$(zenity --entry --title="Song title" --text="Enter the song title:")
-	{ curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title" ; echo "Artist - $artist" ; echo "Title - $title" ; } | zenity --text-info --width=500 --height=500
-else
-	artist="$1"
-	title="$2"
-	{ curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title" ; echo "Artist - $artist" ; echo "Title - $title" ; }
-fi
+main () {
+    if [ -z "$1" ]; then
+        programisinstalled "zenity"
+        if [ "$return" = "1" ]; then
+            artist=$(zenity --entry --title=Artist --text="Enter the artist name:")
+            title=$(zenity --entry --title="Song title" --text="Enter the song title:")
+            { echo "Artist - $artist" ; echo "Title - $title" ; curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title" ; } | zenity --text-info --width=500 --height=500
+        else
+            read -p "Artist: " artist
+            read -p "Title: " title
+            { curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title" ; }
+        fi
+    else
+        artist="$1"
+        title="$2"
+        { echo "Artist - $artist" ; echo "Title - $title" ; curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title" ; }
+    fi
+}
 
+programisinstalled () {
+  # set to 1 initially
+  return=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { return=0; }
+  # return value
+}
+
+programisinstalled "curl"
+if [ "$return" = "1" ]; then
+    main "$1" "$2"
+else
+    echo "'curl' is not installed; cannot fetch lyrics."
+fi
