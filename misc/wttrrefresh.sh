ef708fc71051045ce0c558939ed3589db5d92000
simoniz0r <bnu.fantasma@gmail.com>
2017-04-04 13:03:40 -0500

    update description

--- misc/wttrrefresh.sh
+++ misc/wttrrefresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Refresh wttr.in/$LOCATION?0?Q (LOCATION="$1") every 5 minutes
 # Only tested with city name and zipcode; additional location info should work.
-# Displays only current weather by default; use 'wttr f LOCATION' to get the full output; $location will be used if no LOCATION is input.
+# Displays only current weather by default; use './wttrrefresh.sh f LOCATION' to get the full output; $location will be used if no LOCATION is input.
 # This script uses 'printf '\e[?25l'' to hide the cursor; use 'reset' after running this script if your cursor is still hidden.
 # wttrrefresh.sh - http://www.simonizor.gq/scripts
 
