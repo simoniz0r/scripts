cc0761d1d88862b00976b07c20bf1b460e648078
simoniz0r <bnu.fantasma@gmail.com>
2017-04-04 03:10:02 -0500

    update description

--- misc/wttrrefresh.sh
+++ misc/wttrrefresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Refresh wttr.in every 5 minutes
+# Refresh wttr.in/$LOCATION?0?Q (LOCATION="$1") every 5 minutes
 # Only tested with city name and zipcode; additional location info should work.
 # This script uses 'printf '\e[?25l'' to hide the cursor; use 'reset' after running this script if your cursor is still hidden.
 # wttrrefresh.sh - http://www.simonizor.gq/scripts
