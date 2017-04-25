baf14f668a9402d13db11d3c3c7eaf5982111b26
simoniz0r <bnu.fantasma@gmail.com>
2017-04-24 22:19:44 -0500

    use zenity for pass input during decription

--- gpgpassman/gpgpassman.sh
+++ gpgpassman/gpgpassman.sh
@@ -5,8 +5,8 @@
 # Also with 'zenity', you can execuite 'gpgpassman dec' for direct access to decrypting passwords; can be used with a keybind.
 # Written by simonizor 3/22/2017 - http://www.simonizor.gq/gpgpassman
 
-GPMVER="1.2.9"
-X="v1.2.9 - Added check for 'wget' before running updatecheck."
+GPMVER="1.3.0"
+X="v1.3.0 - Use 'zenity' for password input during decription to avoid errors with gpg-agent."
 # ^^Remember to update this and gpmversion.txt every release!
 SCRIPTNAME="$0"
 GPMDIR="$(< ~/.config/gpgpassman/gpgpassman.conf)"
@@ -344,7 +344,13 @@ main () {
                 echo -n "Password cleared from clipboard" | xclip -selection c -i
                 echo "Password cleard from clipboard."
             elif [ "$ZHEADLESS" = "1" ]; then
-                echo -n "$(gpg -d $SERVNAME)" | xclip -selection c -i
+                PASSINPUT=$(zenity --entry --hide-text --text="Enter your password for $SERVNAME:")
+                if [[ $? -eq 1 ]]; then
+                    SERVNAME=""
+                    zenitymain
+                    exit 0
+                fi
+                echo -n "$(gpg -d --passphrase $PASSINPUT $SERVNAME)" | xclip -selection c -i
                 if [ "$(xclip -selection c -o)" = "" ]; then
                     zenity --error --text="Wrong password or gpg failure!"
                     SERVNAME=""
