f1de3f154dd9134b85d14550a1ead5d6eba8c7da
simoniz0r <bnu.fantasma@gmail.com>
2017-05-08 22:13:02 -0500

    added list-options to arguments entry

--- misc/mpv-zui.sh
+++ misc/mpv-zui.sh
@@ -7,8 +7,8 @@
 
 
 SCRIPTNAME="$0"
-main () {
-    MPVFILE=$(zenity --entry --title=mpv-zui --entry-text="Input a file or click OK to browse for a local file" --text="Input the path to a local file or input a remote url.")
+mpvfile () {
+    MPVFILE=$(zenity --entry --cancel-label="Exit mpv-zui" --title=mpv-zui --entry-text="Input a file or click OK to browse for a local file" --text="Input the path to a local file or input a remote url.")
     if [[ $? -eq 1 ]]; then
         exit 0
     fi
@@ -18,12 +18,26 @@ main () {
             exit 0
         fi
     fi
-    MPVARGS=$(zenity --entry --title=mpv-zui --text="Input the arguments that you would like to run mpv with:" --entry-text="--border=yes --vo=opengl --hwdec=vaapi --cache=no --cache-pause=no --cache-secs=0")
+    mpvargs
+}
+
+mpvargs () {
 # This doesn't work because zenity; figure out how to fix it to make more convenient:    MPVARGS=$(zenity --list --multiple --editable --text="Select arguments to run mpv with" --title="mpv-zui" --column="Arguments" "--vo=opengl" "--hwdec=vaapi") # List commonly used arguments for selection, maybe have it be editable with a couple of blank entries that can be custom arguments
-    if [[ $? -eq 1 ]]; then
-        exit 0
+    MPVARGS=$(zenity --entry --title=mpv-zui --cancel-label="List options" --text="Input the arguments that you would like to run mpv with:" --entry-text="--border=yes --vo=opengl --hwdec=vaapi --cache=no --cache-pause=no --cache-secs=0")
+        if [[ $? -eq 1 ]]; then
+            mpv --list-options | zenity --text-info --cancel-label="Exit mpv-zui" --ok-label="Back" --width=710 --height=600 && mpvargs
+        if [[ $? -eq 1 ]]; then
+            exit 0
+        fi
     fi
-    mpv $MPVARGS "$MPVFILE" && main
+    mpvrun
+}
+
+mpvrun () {
+    mpv $MPVARGS "$MPVFILE"
+    MPVARGS=""
+    MPVFILE=""
+    mpvfile
 }
 
 programisinstalled () { # check if inputted program is installed using 'type'
@@ -35,7 +49,7 @@ programisinstalled "zenity"
 if [ "$return" = "1" ]; then
     programisinstalled "mpv"
     if [ "$return" = "1" ]; then
-        main
+        mpvfile
     else
         echo "mpv is not installed!"
     fi
