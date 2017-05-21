dc8e0c9fbe757558c48ec5c00683e55e4097438b
simoniz0r <bnu.fantasma@gmail.com>
2017-05-21 04:04:36 -0500

    added clears and changed reads so they exit with any key press

--- misc/cinnabuntu.sh
+++ misc/cinnabuntu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Installs cinnamon-desktop-environment on Ubuntu 16.04 or later
 
+clear
 echo "Updating package list..."
 sudo apt update || { echo "Update failed; make sure you are connected to the internet and try again." ; exit 1 ; }
+clear
 echo "Upgrading already installed packages..."
 sudo apt upgrade -y || { echo "Upgrade failed; make sure you are connected to the internet and try again." ; exit 1 ; }
-echo "Installing Cinnamon Desktop Environment..."
+clear
+read -p -n 1 -r "Installing Cinnamon Desktop Environment; this will take several minutes.  Press any key to continue."
 sudo apt install cinnamon-desktop-environment -y || { echo "Cinnamon Desktop Environment install failed; make sure you are connected to the internet and try again.." ; exit 1 ; }
-read -p "Cinnamon Desktop Environment Installed!  Press any key to restart. "
+clear
+read -p -n 1 -r "Cinnamon Desktop Environment Installed!  Press any key to restart. "
 shutdown -r now
