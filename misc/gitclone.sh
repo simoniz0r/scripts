e68e568dd37f62956965b6fba8f103a3bf27361f
simoniz0r <bnu.fantasma@gmail.com>
2017-05-29 01:05:54 -0500

    Changed dirs

--- misc/gitclone.sh
+++ misc/gitclone.sh
@@ -4,15 +4,17 @@
 # bash -c "$(wget --quiet https://raw.githubusercontent.com/simoniz0r/UsefulScripts/master/misc/gitclone.sh -O -)"
 
 gitcheck () {
-    if [ -d "/home/$USER/$1" ]; then
+    if [ -d "/home/$USER/github/$1" ]; then
         read -p "$1 exists; remove before cloning? Y/N "
         if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-            rm -rf "$1" && echo "$1 removed!" || echo "$1 remove failed!"
+            rm -rf "/home/$USER/github/$1" && echo "$1 removed!" || echo "$1 remove failed!"
+            cd /home/$USER/github
             git clone https://github.com/simoniz0r/"$1".git
         else
             echo "$1 not removed; skipping..."
         fi
     else
+        cd /home/$USER/github
         git clone https://github.com/simoniz0r/"$1".git
     fi
 }
