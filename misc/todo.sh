087397d60223894a8ae054bd1d457cdefc25c705
simoniz0r <bnu.fantasma@gmail.com>
2017-06-17 20:31:09 -0500

    Added done argument to mark items with an X to indicate they are done.

--- misc/todo.sh
+++ misc/todo.sh
@@ -10,6 +10,7 @@ Options:
     todo                        # Lists items in todo.list or specified list
     todo add 'Item'             # Adds Item to todo.list or specified list
     todo edit                   # Opens the default editor to edit todo.list or specified list
+    todo done                   # Marks an item on todo.list or specified list with an X to indicate it is done
     todo delete 'Item'          # Deletes Item from todo.list or specified list
 
 Examples:
@@ -17,6 +18,8 @@ Examples:
     todo custom                 # Lists all items in custom.list
     todo add 'Item'             # Adds Item to todo.list
     todo add custom 'Item'      # Adds Item to custom.list
+    todo done 'Item'            # Marks Item on todo.list with an X to indicate it is done.
+    todo done custom 'Item'     # Marks Item on custom.list with an X to indicate it is done.
     todo edit custom            # Opens the default editor to edit custom.list
     todo delete 'Item'          # Deletes Item from todo.list
     todo delete custom 'Item'   # Deletes Item from custom.list
@@ -66,6 +69,52 @@ todo () {
             else
                 $EDITOR ~/.todo/$2.list
                 cat ~/.todo/$2.list
+            fi
+            ;;
+        done|finished)
+            if [ ! -z "$4" ]; then
+                echo "Use single or double quotes when marking items as done."
+                echo "Ex: todo done category 'Item to mark as done'"
+                exit 1
+            fi
+            if [ -z "$3" ]; then
+                TODOITEM="$(grep -a "$2" ~/.todo/todo.list)"
+                DELNUM="$(echo "$TODOITEM" | wc -l)"
+                if ! grep -q "$2" ~/.todo/todo.list; then
+                    echo "$2 not found in todo!"
+                    exit 1
+                fi
+                if [[ "$DELNUM" != "1" ]]; then
+                    echo "$DELNUM results found; refine your input."
+                    echo "$TODOITEM"
+                    exit 1
+                fi
+                TODOITEM="${TODOITEM:1}"
+                sed -i s:-"$TODOITEM":X"$TODOITEM":g ~/.todo/todo.list
+                TODOLIST="$(cat ~/.todo/todo.list)"
+                echo
+                echo "todo:"
+                cat ~/.todo/todo.list
+                echo
+            else
+                TODOITEM="$(grep -a "$3" ~/.todo/$2.list)"
+                DELNUM="$(echo "$TODOITEM" | wc -l)"
+                if ! grep -q "$3" ~/.todo/$2.list; then
+                    echo "$3 not found in $2!"
+                    exit 1
+                fi
+                if [[ "$DELNUM" != "1" ]]; then
+                    echo "$DELNUM results found; refine your input."
+                    echo "$TODOITEM"
+                    exit 1
+                fi
+                TODOITEM="${TODOITEM:1}"
+                sed -i s:-"$TODOITEM":X"$TODOITEM":g ~/.todo/$2.list
+                TODOLIST="$(cat ~/.todo/$2.list)"
+                echo
+                echo "$2:"
+                cat ~/.todo/$2.list
+                echo
             fi
             ;;
         delete|remove)
