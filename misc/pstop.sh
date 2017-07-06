ad8e2f33a6dec0ae3db4917cdd26ff72f25a0331
simoniz0r <bnu.fantasma@gmail.com>
2017-07-06 00:12:09 -0500

    Added proper sorting and fixed up arguments. Will add sorting argument later

--- misc/pstop.sh
+++ misc/pstop.sh
@@ -4,18 +4,18 @@ pad="$(printf '%0.1s' "-"{1..60})"
 prepad="$(printf '%-23s')"
 padlength1=17
 padlength2=7
+SORTNUM="5,5"
 
 main () {
-    # TOPH="$(top -bHn 1 -o +%MEM | tail -n +8)"
-    PSCOMMS="$(ps -eo comm --sort=-%mem --no-headers | tr -d ' ')"
-    echo "${prepad}COMMAND            %CPU   %MEM   COUNT"
+    PSCOMMS="$(ps -eo comm --sort=-%cpu --no-headers | tr -d ' ')"
+    printf "${prepad}COMMAND            %%CPU   %%MEM   COUNT\n"
     for command in $(echo "$PSCOMMS" | awk '!seen[$1]++' | head -n "$HEADNUM"); do
         if [ "$command" = "WebContent" ]; then
-            command="Web Content"
+            command="Web"
         fi
-        CPU=" $(ps -eo %cpu,comm --sort=-%mem --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
-        MEM=" $(ps -eo %mem,comm --sort=-%mem --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
-        COUNT=" $(ps -eo comm --sort=-%mem --no-headers | grep "$command" | wc -l)"
+        CPU=" $(ps H -eo %cpu,comm --sort=-%cpu --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
+        MEM=" $(ps -eo %mem,comm --sort=-%cpu --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
+        COUNT=" $(ps -eo comm --sort=-%cpu --no-headers | grep "$command" | wc -l)"
         printf '%s' "$prepad" "$command "
         printf '%*.*s' 0 $((padlength1 - ${#command} )) "$pad"
         printf '%s' "$CPU"
@@ -23,22 +23,30 @@ main () {
         printf '%s' "$MEM"
         printf '%*.*s' 0 $((padlength2 - ${#MEM} )) "$pad"
         printf '%s\n' "-$COUNT"
-    done
+    done | sort -h -k "$SORTNUM" -r
     CPU=""
     MEM=""
     NUM=""
 }
 
 case $1 in
-    1*|2*|3*|4*|5*|6*|7*|8*|9*)
-        HEADNUM="$1"
-        main
+    -l*|--l*)
+        case $2 in
+            1*|2*|3*|4*|5*|6*|7*|8*|9*)
+                HEADNUM="$2"
+                main
+                ;;
+            *)
+                echo "$2 is not a number!"
+                exit 1
+                ;;
+        esac
         ;;
     -w*|--w*)
         if [ -z "$2" ]; then
             watch -tn 1 "$0" main
         else
-            watch -tn 1 "$0" "$2"
+            watch -tn 1 "$0" "$2" "$3"
         fi
         ;;
     *)
