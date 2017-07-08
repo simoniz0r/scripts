38f1da5e04dd1e533898431a50b963c24bdbcf95
simoniz0r <bnu.fantasma@gmail.com>
2017-07-08 01:04:59 -0500

    Added most of the arguments. -r=n chanes number of rows displayed -p=n changes the padding in front of output -c=<character> changes the character used as padding -w changes the character used as padding to whitespace.  Will more than likely display as an error character in your terminal unless used with the 'watch' command. -s=cpu|mem changes sorting to be either cpu or mem Will add -h argument later to display this output in pstop.sh

--- misc/pstop.sh
+++ misc/pstop.sh
@@ -1,56 +1,82 @@
 #!/bin/bash
 
 pad="$(printf '%0.1s' "-"{1..60})"
-prepad="$(printf '%-23s')"
-padlength1=17
-padlength2=7
-SORTNUM="5,5"
+prepad="$(printf '%-0s')"
+padlength1=19
+padlength2=9
+HEADNUM="10"
+SORTNUM="3,3"
+SORTPERCENT="%cpu"
 
 main () {
-    PSCOMMS="$(ps -eo comm --sort=-%cpu --no-headers | tr -d ' ')"
-    printf "${prepad}COMMAND            %%CPU   %%MEM   COUNT\n"
+    PSCOMMS="$(ps -eo comm --sort=-"$SORTPERCENT" --no-headers | tr -d ' ')"
+    printf "${prepad}COMMAND              %%CPU     %%MEM    COUNT\n"
     for command in $(echo "$PSCOMMS" | awk '!seen[$1]++' | head -n "$HEADNUM"); do
         if [ "$command" = "WebContent" ]; then
             command="Web"
         fi
-        CPU=" $(ps H -eo %cpu,comm --sort=-%cpu --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
-        MEM=" $(ps -eo %mem,comm --sort=-%cpu --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
-        COUNT=" $(ps -eo comm --sort=-%cpu --no-headers | grep "$command" | wc -l)"
+        CPU=" $(ps H -eo %cpu,comm --sort=-"$SORTPERCENT" --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
+        MEM=" $(ps -eo %mem,comm --sort=-"$SORTPERCENT" --no-headers | grep "$command" | awk '{ SUM += $1} END { print SUM }') "
+        COUNT=" $(ps -eo comm --sort=-"$SORTPERCENT" --no-headers | grep "$command" | wc -l)"
         printf '%s' "$prepad" "$command "
         printf '%*.*s' 0 $((padlength1 - ${#command} )) "$pad"
         printf '%s' "$CPU"
         printf '%*.*s' 0 $((padlength2 - ${#CPU} )) "$pad"
         printf '%s' "$MEM"
         printf '%*.*s' 0 $((padlength2 - ${#MEM} )) "$pad"
-        printf '%s\n' "-$COUNT"
-    done | sort -h -k "$SORTNUM" -r
-    CPU=""
-    MEM=""
-    NUM=""
+        printf '%s\n' "$COUNT"
+    done | sort -n -k "$SORTNUM" -r
 }
 
-case $1 in
-    -l*|--l*)
-        case $2 in
-            1*|2*|3*|4*|5*|6*|7*|8*|9*)
-                HEADNUM="$2"
-                main
-                ;;
-            *)
-                echo "$2 is not a number!"
-                exit 1
-                ;;
-        esac
-        ;;
-    -w*|--w*)
-        if [ -z "$2" ]; then
-            watch -tn 1 "$0" main
-        else
-            watch -tn 1 "$0" "$2" "$3"
-        fi
-        ;;
-    *)
-        HEADNUM="10"
-        main
-        ;;
-esac
+for arg in "$@"; do
+    case $arg in
+        -r=*)
+            case ${arg:3} in
+                1*|2*|3*|4*|5*|6*|7*|8*|9*)
+                    HEADNUM="${arg:3}"
+                    ;;
+                *)
+                    echo "${arg:3} is not a valid number choice!"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        -p=*)
+            case ${arg:3} in
+                0*|1*|2*|3*|4*|5*|6*|7*|8*|9*)
+                    PREPADNUM="%-${arg:3}s"
+                    prepad="$(printf "$PREPADNUM")"
+                    ;;
+                *)
+                    echo "${arg:3} is not a valid number choice!"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        -w)
+            PADCHAR=" "
+            pad="$(printf '%0.1s' "$PADCHAR"{1..60})"
+            ;;
+        -c=*)
+            PADCHAR="${arg:3}"
+            pad="$(printf '%0.1s' "$PADCHAR"{1..60})"
+            ;;
+        -s=*)
+            case ${arg:3} in
+                cpu)
+                    SORTNUM="3,3"
+                    SORTPERCENT="%cpu"
+                    ;;
+                mem)
+                    SORTNUM="5,5"
+                    SORTPERCENT="%mem"
+                    ;;
+                *)
+                    echo "${arg:3} is not a valid choice!"
+                    exit 1
+                    ;;
+            esac
+            ;;
+    esac
+done
+main
