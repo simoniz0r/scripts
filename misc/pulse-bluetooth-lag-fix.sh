a5e9cda0ad77db58ba9c3465b5b59080887f172c
simoniz0r <bnu.fantasma@gmail.com>
2017-09-17 19:23:51 -0500

    Add pulse-bluetooth-lag-fix.sh

---
+++ misc/pulse-bluetooth-lag-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A script to get rid of audio delay when using bluetooth with pulseaudio
+# Modified version of : https://askubuntu.com/questions/145935/get-rid-of-0-5s-latency-when-playing-audio-over-bluetooth-with-a2dp
+
+BLUEZCARD="$(pactl list cards | grep 'Name:*..*alsa' | cut -f2 -d':' | tr -d ' ')"
+pactl set-card-profile "$BLUEZCARD" a2dp_sink
+pactl set-card-profile "$BLUEZCARD" headset_head_unit
+pactl set-card-profile "$BLUEZCARD" a2dp_sink
+SINK="$(pactl list cards | grep -A 4 'Name:*..*alsa' | grep 'alsa.card =' | cut -f2 -d'"')
+pacmd set-default-sink "$SINK"
